c56c52b6bf2a9405d10ee676a2896028003d4523
ELdotcss <74616972+ELdotcss@users.noreply.github.com>
2020-11-20 19:36:13 +0100

    Revert "Css"

--- login.php
+++ login.php
@@ -1,6 +1,3 @@
-<style type="text/css">
-<?php include 'css/login.css'; ?>
-</style>
 <?php
 session_start();
 include 'header.php';
@@ -49,7 +46,7 @@ if(isset($_POST['login']))
 </head>
 <body>
 <form action="login.php" method="post">
-    <h2 class="bordsRonds">Connexion</h2>
+    <h2>Connexion</h2>
     <p><input type="email" name="email_log" placeholder="Email etudiant" value="<?php if(isset($email_log)) {echo $email_log; }?>"/></p>
     <p><input type="password" name="password_log" placeholder="Mot de passe"/></p>
     <?php if(isset($erreur)){echo '<font color="red">'. $erreur;};?>
