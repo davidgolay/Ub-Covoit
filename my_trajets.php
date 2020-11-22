5e94e6f2bf8b38075de785cb2c365c8c46fe2c06
davidgolay <74565736+davidgolay@users.noreply.github.com>
2020-11-22 02:33:37 +0100

    profil et trajet update
    edit_profil.php:
    - tout les champs fonctionnels
    - html corrigé formulaire passé en tableau
    
    my_trajet.php:
    - affichage des trajets plus propre
    - affichage du point de départ et d'arrivée
    -affichage des conducteurs avec redirection vers leur profil
    
    profil.php:
    - affichage de la bio

--- my_trajets.php
+++ my_trajets.php
@@ -10,7 +10,7 @@ if($_SESSION['is_driver'] == 1)
     echo '<div class="button"><a href="my_trajets_driver.php">Mes trajets en tant que conducteur</a>';
 }
 
-$incoming_trajet = $bdd->prepare("SELECT date_format(datetime_trajet, '%d/%m/%Y') as date, 
+$incoming_trajet = $bdd->prepare("SELECT trajet.partir_ub, trajet.id_trajet, trajet.id_user, date_format(datetime_trajet, '%d/%m/%Y') as date, 
 date_format(datetime_trajet, '%h:%i') as hour, nom, prenom, is_driver 
 FROM trajet INNER JOIN users ON users.id = trajet.id_user 
 INNER JOIN participe ON trajet.id_trajet = participe.id_trajet 
@@ -18,29 +18,41 @@ WHERE participe.id_user = ? AND trajet.datetime_trajet > ? ;");
 $incoming_trajet->execute(array($_SESSION['id'], $date_now));
 
 
-echo '<h2>Trajets à venir</h2>';
+echo '<h2>Trajets à venir :</h2>';
 
 foreach($incoming_trajet as $row)
 {
-    echo '<div> Trajet de ' . $row['prenom'] . ' ' . $row['nom'] . ', le ' . $row['date'] . ' à ' . $row['hour'] . '</div>';
+    $depart = $row['partir_ub'];
+    $trajet = $row['id_trajet'];
+    $driver = $row['id_user'];
+    $ville = $bdd->prepare("SELECT ville_nom_reel FROM trajet 
+    INNER JOIN ville ON trajet.id_ville = ville.id_ville 
+    WHERE id_trajet = ?;");
+    $ville->execute(array($trajet));
+    $nom_ville = $ville->fetch();
+
+    if($depart == 1)
+    {
+        echo ' 
+        <div>
+            <p> Le ' . $row['date'] . ' à ' . $row['hour'] . '</p>
+            <p> De uB, à :'. $nom_ville['ville_nom_reel'] . '</p>
+            <p> Conducteur :<a href="profil.php?id=' . $driver.'">'. $row['prenom'] . ' ' . $row['nom'] . '</a></p>
+        </div></br>';
+    }
+    else
+    {
+        echo ' 
+        <div>
+            <p> Le ' . $row['date'] . ' à ' . $row['hour'] . '</p>
+            <p> De ' . $nom_ville['ville_nom_reel'] . ' à uB </p>
+            <p> Conducteur :<a href="profil.php?id=' . $driver.'">'. $row['prenom'] . ' ' . $row['nom'] . '</a></p>
+        </div></br>';
+    }
     
 }
 
 
-$done_trajet = $bdd->prepare("SELECT date_format(datetime_trajet, '%d/%m/%Y') as date, 
-date_format(datetime_trajet, '%h:%i') as hour, nom, prenom, is_driver 
-FROM trajet INNER JOIN users ON users.id = trajet.id_user 
-INNER JOIN participe ON trajet.id_trajet = participe.id_trajet 
-WHERE participe.id_user = ? AND trajet.datetime_trajet < ? ;");
-$done_trajet->execute(array($_SESSION['id'], $date_now));
-
-echo '<h2>Trajets effectués</h2>';
-
-foreach($done_trajet as $row2)
-{
-    echo '<div> Trajet de ' . $row2['prenom'] . ' ' . $row2['nom'] . ', le ' . $row2['date'] . ' à ' . $row2['hour'] . '</div>';
-    
-}
 
 
 ?>
