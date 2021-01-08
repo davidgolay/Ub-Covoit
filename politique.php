279f9f041d3316daaa211100537d8ed4631e2caf
Arkcoz <lucas.mnt21@gmail.com>
2021-01-08 21:15:30 +0100

    Page Juridique CSS PHP
    Les 3 Pages Juridiques CSS+PHP faites

--- politique.php
+++ politique.php
@@ -1,96 +1,87 @@
-<h1>Politique de confidentialité</h1>
-<div><br></div>
-<div><strong>Introduction</strong></div>
-<div>Devant le développement des nouveaux outils de communication, 
-il est nécessaire de porter une attention particulière à la protection de la vie privée. 
-C’est pourquoi, nous nous engageons à respecter la confidentialité des renseignements personnels que nous collectons.</div>
-<div><br></div>
+<?php
+session_start();
+include 'header.php';
+?>
+<div id="page">
+    <h1>Politique de confidentialité</h1>
 
-<h2>Collecte des renseignements personnels</h2>
+    <h2>1. Généralités</h2>
 
-<div>
-<ul>
-    <li>Prénom</li>
-    <li>Adresse postale</li>
-    <li>Code postal</li>
-    <li>Adresse électronique étudiante</li>
-    <li>Adresse électronique personnelle</li>
-    <li>Numéro de téléphone / télécopieur</li>
-</ul>
-</div>
-<div>Les renseignements personnels que nous collectons sont recueillis au travers 
-de formulaires et grâce à l’interactivité établie entre vous et notre site Web. 
-Nous utilisons également, comme indiqué dans la section suivante, 
-des fichiers témoins et/ou journaux pour réunir des informations vous concernant.</div>
-<div><br></div><h2>Formulaires&nbsp;et interactivité:</h2>
+    <p>La société Ub’Covoit, siégeant Boulevard Docteur Petitjean, 21000 Dijon, en sa qualité de responsable du traitement, attache une grande importance à la protection et au respect de votre vie privée. La présente politique vise à vous informer de nos pratiques concernant la collecte, l’utilisation et le partage des informations que vous êtes amenés à nous fournir par le biais de notre plateforme (la « Plateforme ») accessible depuis le site internet <a href=http://grp-366.iq.iut21.u-bourgogne.fr/siteub/page/login.php>http://grp-366.iq.iut21.u-bourgogne.fr/siteub/page/login.php</a>. 
+    Cette politique (ainsi que nos Conditions générales d’utilisation) présente la manière dont nous traitons les données personnelles que nous recueillons et que vous nous fournissez. Nous vous invitons à lire attentivement le présent document pour connaître et comprendre nos pratiques quant aux traitements de vos données personnelles que nous mettons en œuvre.
+    <h2>2. Les informations que nous recueillons</h2>
 
-<div>Vos renseignements personnels sont collectés par le biais de formulaire, à savoir :</div>
-<div><br></div>
-<div>
-<ul>
-    <li>Formulaire d'inscription au site Web</li>
-</ul>
-</div>
-<div>Nous utilisons les renseignements ainsi collectés pour les finalités suivantes :</div>
-<div><br></div>
-<div>
-<ul>
-    <li>Statistiques</li>
-</ul>
-</div>
-<div>Vos renseignements sont également collectés par le biais de l’interactivité 
-pouvant s’établir entre vous et notre site Web et ce, de la façon suivante:</div>
-<div><br></div>
-<div>
-<ul>
-    <li>Commentaires </li>
-</ul>
-</div>
-<div>Nous utilisons les renseignements ainsi collectés pour les finalités suivantes :</div>
-<div><br></div>
-<div>
-<ul>
-    <li>Contact </li>
-</ul>
-</div>
-<div><br></div>
-<h2>Droit d’opposition et de retrait</h2>
-<div>Nous nous engageons à vous offrir un droit d’opposition et de retrait quant à vos renseignements personnels.</div>
-<div>Le droit d’opposition s’entend comme étant la possiblité offerte aux internautes de refuser 
-que leurs renseignements personnels soient utilisées à certaines fins mentionnées lors de la collecte.</div>
-<div><br></div>
-<div>Le droit de retrait s’entend comme étant la possiblité offerte aux internautes de demander 
-à ce que leurs renseignements personnels ne figurent plus, par exemple, dans une liste de diffusion.</div>
-<div><br></div>
-<div><strong>Pour pouvoir exercer ces droits, vous pouvez :</strong></div>
-<div><br></div>
-<div><br></div>
-<h2>Droit d’accès</h2>
-<div>Nous nous engageons à reconnaître un droit d’accès et de rectification aux personnes concernées désireuses de consulter, 
-modifier, voire radier les informations les concernant.</div>
-<div><br></div>
-<div><strong>L’exercice de ce droit se fera :</strong></div>
-<div><br></div>
-<div></div><div></div><div></div><div></div><div><br></div>
-<h2>Sécurité</h2>
-<div>Les renseignements personnels que nous collectons sont conservés dans un environnement sécurisé. 
-Les personnes travaillant pour nous sont tenues de respecter la confidentialité de vos informations.</div>
-<div><br></div>
-<div>Pour assurer la sécurité de vos renseignements personnels, nous avons recours aux mesures suivantes :</div>
-<div><br></div>
-<div>
-<ul>
-<li>Sauvegarde informatique</li>
-<li>Identifiant / mot de passe</li>
-</ul>
-</div>
-<div>Nous nous engageons à maintenir un haut degré de confidentialité en intégrant les dernières innovations technologiques permettant d’assurer la confidentialité de vos transactions. 
-Toutefois, comme aucun mécanisme n’offre une sécurité maximale, une part de risque est toujours présente lorsque l’on utilise Internet pour transmettre des renseignements personnels.</div>
-<div><br></div>
+    <p>Nous sommes susceptibles de recueillir et de traiter les données suivantes :</p>
 
-<h2>Législation</h2>
-<div>Nous nous engageons à respecter les dispositions législatives énoncées dans :</div>
-<div><br></div>
-<div>Législation:  Règlement général sur la protection des données - RGPD</div>
-<div><br></div>
-<div><strong>Générateur de politique de confidentialité / </strong><a href="//www.politiquedeconfidentialite.ca" target="_blank"><strong>Politique de confidentialité</strong></a></div>
+    <h3>2.1. Les informations que vous nous transmettez directement</h3>
+
+    <p>En utilisant nos Plateformes, vous êtes amenés à nous transmettre des informations, dont certaines sont de nature à vous identifier (« Données Personnelles »). C’est notamment le cas lorsque vous remplissez des formulaires (comme par exemple le formulaire d’inscription.
+    Ces informations contiennent notamment les données suivantes :</p>
+    
+    <p>2.1.1. Les données nécessaires à l’inscription au service que nous fournissons sur nos Plateformes ou à l’accès à tout autre service fournis par nous. Ces données sont notamment vos nom et prénom, adresse e-mail, date de naissance, sexe, numéro de téléphone et mot de passe. Ces informations sont obligatoires. A défaut, Ub’Covoit ne sera pas en mesure de vous fournir les services proposés par nos Plateformes et vous ne serez pas autorisés à créer de compte sur notre Plateforme ;</p>
+    
+    <p>2.1.2. Des informations relatives à votre véhicule ;</p>
+    
+    <p>2.1.3. Votre mini-biographie ;</p>
+    
+    <p>2.1.4. Une copie de tous les échanges intervenus entre vous et Ub’Covoit ;</p>
+    
+    <p>2.1.5. Une copie de l’ensemble des réservations ou des publications effectuées sur nos Plateformes ;</p>
+    
+    <h3>2.2. Durée de conservation de vos données</h3>
+   
+    <p>2.2.1. A l’exception des catégories de Données Personnelles, vos Données Personnelles sont archivées à l’issue des périodes suivantes:
+    •	5 ans après votre dernière utilisation de nos Plateformes, si vous n’avez pas fermé votre compte;
+    •	1 an après la fermeture de votre compte, sauf si vous avez reçu un avis négatif ou un signalement, auquel cas vos données sont conservées pendant 2 ans suivant le dernier avis négatif ou signalement reçu si cette durée est plus longue.</p>
+
+    <p>2.2.2. Dans l’hypothèse où votre compte a été suspendu ou bloqué, nous conservons vos données pour une durée variant de 2 à 10 ans à compter de la suspension afin d’éviter tout contournement de votre part des règles en vigueur sur nos Plateformes.</p>
+    
+    <h2>3. Vos données sont-elles transférées, comment et où ?</h2>
+
+    <p>En règle générale, nous conservons vos Données Personnelles au sein de la France.</p> 
+
+    <h2>4. Quels sont vos droits sur vos données personnelles ?</h2>
+
+    <p>4.1. Vous disposez du droit de recevoir une copie de vos Données Personnelles en notre possession (« droit d’accès »).</p>
+    
+    <p>4.2. Vous pouvez également demander l’effacement de vos Données Personnelles ainsi que la rectification des Données Personnelles erronées ou obsolètes (« droit d’effacement et droit de rectification »). Veuillez noter que nous sommes susceptibles de conserver certaines informations vous concernant lorsque la loi nous l’impose ou lorsque nous avons un motif légitime de le faire. C’est par exemple le cas, si nous estimons que vous avez commis une fraude ou violé nos Conditions Générales d’Utilisation et que nous souhaitons éviter que vous contourniez les règles applicables à notre communauté.</p>
+    
+    <p>4.3. Vous disposez également du droit de vous opposer à tout moment pour des raisons tenant à sa situation particulière, (i) au traitement de vos Données Personnes à des fins de marketing direct ou aux traitements effectués sur le fondement de notre intérêt légitime (« droit d’opposition »).</p>
+    
+    <p>4.4. Vous disposez du droit de limiter les traitements effectués sur vos Données Personnelles (« droit à la limitation »). Veuillez noter que ce droit ne s’applique que si (i) vous contestez l’exactitude de vos Données Personnelles pendant la durée nous permettant de vérifier l’exactitude de ces dernières; (ii) en cas de traitement illicite de notre part et que vous exigez une limitation de leur utilisation plutôt qu’un effacement, (iii) nous n’avons plus besoin des données à caractère personnel aux fins du traitement mais celles-ci vous sont encore nécessaires pour la constatation, l’exercice ou la défense de droits en justice; iv) en cas d’exercice de votre droit d’opposition pendant la durée de vérification portant sur le point de savoir si les motifs légitimes que nous poursuivons prévalent les votre.</p>
+   
+    <p>4.5. Vous disposez du droit d’introduire une réclamation auprès de l’autorité de contrôle compétente ou d’obtenir réparation auprès des tribunaux compétents si vous considérez nous n’avons pas respecté vos droits.</p>
+    
+    <p>4.6. Vous disposez également du droit à la portabilité de vos données, c’est-à-dire au droit de recevoir les Données Personnelles que vous nous avez fournis dans un format structuré, couramment utilisé et lisible par la machine et le droit de transmettre ces données à un autre responsable du traitement (« droit à la portabilité »).</p>
+    
+    <p>4.7. Vous disposez également du droit de définir des directives relatives au sort de vos Données Personnelles après votre mort.</p>
+    
+    <p>4.8. Pour exercer ces droits, vous pouvez contacter notre Délégué à la Protection des Données Personnelles selon les modalités définies à l’article 8.</p>
+    
+    <h2>5. Confidentialité de votre mot de passe</h2>
+    
+    <p>Vous êtes responsable de la confidentialité du mot de passe que nous vous avez choisi pour accéder à votre compte sur nos Plateformes.</p>
+    
+    <p>Vous vous engagez à conserver ce mot de passe secret et à ne le communiquer à personne.</p>
+    
+    <h2>6. Liens vers d’autres sites internet et réseaux sociaux</h2>
+    
+    <p>Nos Plateformes peuvent occasionnellement contenir des liens vers les sites internet de nos partenaires ou de sociétés tierces. Veuillez noter que ces sites internet ont leur propre politique de confidentialité et que nous déclinons toute responsabilité quant à l’utilisation faite par ces sites des informations collectées lorsque vous cliquez sur ces liens. Nous vous invitons à prendre connaissance de politiques de confidentialité de ces sites avant de leur transmettre vos Données Personnelles.</p>
+    
+    <h2>7. Modification de notre politique de confidentialité</h2>
+    
+    <p>Nous pouvons être amené à modifier occasionnellement la présente politique de confidentialité. Lorsque cela est nécessaire, nous vous en informerons et/ou solliciterons votre accord. Nous vous conseillons de consulter régulièrement cette page pour prendre connaissance des éventuelles modifications ou mises à jour apportées à notre politique de confidentialité.
+    
+    <h2>8. Contact</h2>
+   
+    <p>Pour toute question relative à la présente politique de confidentialité ou pour toute demande relative à vos données personnelles, vous pouvez nous contacter en :</p>
+    <div id=phrase><p>•	adressant un email à notre délégué à la protection des données à l’adresse <a href=mailto:dataprotection@ubCovoit.com>dataprotection@ubCovoit.com</a></p></div>
+
+
+    <style>
+    <?php include 'css/politique.css'; ?>
+    </style>
+</div>
+<?php
+include 'footer.php';
+?>
