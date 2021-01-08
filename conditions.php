99ac1edc6b1e4525eb4c92846bd671e51e85e49d
davidgolay <74565736+davidgolay@users.noreply.github.com>
2021-01-08 21:25:26 +0100

    Condition & politique

--- conditions.php
+++ conditions.php
@@ -1,7 +1,131 @@
 <?php
+session_start();
 include 'header.php';
 ?>
+<div id="page">
+    <h1>Conditions générales d'utilisation</h1>
 
+
+
+    <div id=général>En vigueur au 28/12/2020</div>
+    <br>
+    <p>Les présentes conditions générales d'utilisation (dites « CGU ») ont pour objet l'encadrement juridique des modalités de mise à disposition du site et des services par Ub’Covoit et de définir les conditions d’accès et d’utilisation des services par « l'Utilisateur ».</p>
+    <p>Les présentes CGU sont accessibles sur le site à la rubrique «CGU».</p>
+
+    <br>
+    <p>Toute inscription ou utilisation du site implique l'acceptation sans aucune réserve ni restriction des présentes CGU par l’utilisateur. Lors de l'inscription sur le site via le Formulaire d’inscription, chaque utilisateur accepte expressément les présentes CGU en cochant la case précédant le texte suivant : « Je reconnais avoir lu et compris les CGU et je les accepte ».</p>
+    <p>En cas de non-acceptation des CGU stipulées dans le présent contrat, l'Utilisateur se doit de renoncer à l'accès des services proposés par le site.</p>
+    <p>Ub'Covoit se réserve le droit de modifier unilatéralement et à tout moment le contenu des présentes CGU.</p>
+
+
+    <h2>Article 1 : Les mentions légales</h2>
+
+
+    <p>L'édition du site d'Ub'Covoit est assurée par la Société (fictive) EURL Ub’Covoit, dont le siège social est situé au Boulevard Docteur Petitjean, 21000 Dijon (IUT Informatique - Dijon)</p>
+    <p>Numéro de téléphone  <a href="tel:+33380396550">03 80 39 65 50</a></p>
+    <p>Adresse e-mail : <a href=mailto:David_Golay@etu.u-bourgogne.fr>David_Golay@etu.u-bourgogne.fr</a>.</p>
+    <p>Le Directeur de la publication est : Golay David</p>
+    <p>L'adresse du site internet d'Ub'Covoit est : <a href=http://grp-366.iq.iut21.u-bourgogne.fr/siteub/page/login.php>http://grp-366.iq.iut21.u-bourgogne.fr/siteub/page/login.php</a></p>
+
+
+    <h2>ARTICLE 2 : Accès au site</h2>
+
+
+
+    <p>Le site d'Ub'Covoit permet à l'Utilisateur un accès gratuit aux services suivants :</p>
+    <p>Le site internet propose les services suivants :</p>
+    <p>Communiquer, échanger et fixer des trajets de covoiturage entre les membres.</p>
+    <p>Le site est accessible gratuitement en tout lieu à tout Utilisateur ayant un accès à Internet. Tous les frais supportés par l'Utilisateur pour accéder au service (matériel informatique, logiciels, connexion Internet, etc.) sont à sa charge.</p>
+
+    <br>
+    <p>L’Utilisateur non membre n'a pas accès aux services réservés. Pour cela, il doit s’inscrire en remplissant le formulaire. En acceptant de s’inscrire aux services réservés, l’Utilisateur membre s’engage à fournir des informations sincères et exactes concernant son état civil et ses coordonnées, notamment son adresse email.</p>
+    <p>Pour accéder aux services, l’Utilisateur doit ensuite s'identifier à l'aide de son identifiant et de son mot de passe qui lui seront communiqués après son inscription.</p>
+    <p>Tout Utilisateur membre régulièrement inscrit pourra également solliciter sa désinscription en se rendant à la page dédiée sur son espace personnel. Celle-ci sera effective dans un délai raisonnable.</p>
+    <p>Tout événement dû à un cas de force majeure ayant pour conséquence un dysfonctionnement du site ou serveur et sous réserve de toute interruption ou modification en cas de maintenance, n'engage pas la responsabilité de d'Ub'Covoit. Dans ces cas, l’Utilisateur accepte ainsi ne pas tenir rigueur à l’éditeur de toute interruption ou suspension de service, même sans préavis.</p>
+    <p>L'Utilisateur a la possibilité de contacter le site par messagerie électronique à l’adresse email de l’éditeur communiqué à l’ARTICLE 1.</p>
+
+
+    <h2>ARTICLE 3 : Collecte des données</h2>
+
+
+
+    <p>Le site assure à l'Utilisateur une collecte et un traitement d'informations personnelles dans le respect de la vie privée conformément à la loi n°78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers et aux libertés. Le site est déclaré à la CNIL sous le numéro 000000.</p>
+    <p>En vertu de la loi Informatique et Libertés, en date du 6 janvier 1978, l'Utilisateur dispose d'un droit d'accès, de rectification, de suppression et d'opposition de ses données personnelles. L'Utilisateur exerce ce droit :</p>
+
+
+    <p>·         via son espace personnel ;</p>
+
+
+    <h2>ARTICLE 4 : Propriété intellectuelle</h2>
+
+
+
+    <p>Les marques, logos, signes ainsi que tous les contenus du site (textes, images, son…) font l'objet d'une protection par le Code de la propriété intellectuelle et plus particulièrement par le droit d'auteur.</p>
+
+    <br>
+    <p>L'Utilisateur doit solliciter l'autorisation préalable du site pour toute reproduction, publication, copie des différents contenus. Il s'engage à une utilisation des contenus du site dans un cadre strictement privé, toute utilisation à des fins commerciales et publicitaires est strictement interdite.</p>
+    <p>Toute représentation totale ou partielle de ce site par quelque procédé que ce soit, sans l’autorisation expresse de l’exploitant du site Internet constituerait une contrefaçon sanctionnée par l’article L 335-2 et suivants du Code de la propriété intellectuelle.</p>
+    <p>Il est rappelé conformément à l’article L122-5 du Code de propriété intellectuelle que l’Utilisateur qui reproduit, copie ou publie le contenu protégé doit citer l’auteur et sa source.</p>
+    
+    
+    <h2>ARTICLE 5 : Responsabilité</h2>
+
+
+
+    <p>Les sources des informations diffusées sur le site d'Ub'Covoit sont réputées fiables mais le site ne garantit pas qu’il soit exempt de défauts, d’erreurs ou d’omissions.</p>
+    <p>Les informations communiquées sont présentées à titre indicatif et général sans valeur contractuelle. Malgré des mises à jour régulières, le site d'Ub'Covoit ne peut être tenu responsable de la modification des dispositions administratives et juridiques survenant après la publication. De même, le site ne peut être tenue responsable de l’utilisation et de l’interprétation de l’information contenue dans ce site.</p>
+    <br>
+    <p>L'Utilisateur s'assure de garder son mot de passe secret. Toute divulgation du mot de passe, quelle que soit sa forme, est interdite. Il assume les risques liés à l'utilisation de son identifiant et mot de passe. Le site décline toute responsabilité.</p>
+    <br>
+    <p>En cas de proposition de trajet, la responsabilité de l’Utilisateur de posséder un permis de conduire contenant au minimum un point et une carte d’identité aux normes lui appartient, la plateforme ne pourra vérifier. </p>
+    <br>
+    <p>Ub’Covoit ne sera pas tenu responsable des dérives des utilisateurs, pour cela, nous donnerons un fort degré de responsabilité à nos utilisateurs. Toutefois, Ub’Covoit pourrait éventuellement apporter un système de liste noire des utilisateurs malveillants. </p>
+    <br>
+    <p>Le site  ne peut être tenu pour responsable d’éventuels virus qui pourraient infecter l’ordinateur ou tout matériel informatique de l’Internaute, suite à une utilisation, à l’accès, ou au téléchargement provenant de ce site.</p>
+    <p>La responsabilité du site ne peut être engagée en cas de force majeure ou du fait imprévisible et insurmontable d'un tiers.</p>
+    
+
+    <h2>ARTICLE 6 : Liens hypertextes</h2>
+
+    <p>Des liens hypertextes peuvent être présents sur le site. L’Utilisateur est informé qu’en cliquant sur ces liens, il sortira du site d'Ub'Covoit. Ce dernier n’a pas de contrôle sur les pages web sur lesquelles aboutissent ces liens et ne saurait, en aucun cas, être responsable de leur contenu.</p>
+    
+    
+    <h2>ARTICLE 7 : Cookies</h2>
+
+
+    <p>La plateforme Ub’Covoit n’utilise actuellement aucun cookie, cependant elle réserve la possibilité de faire appel à des cookies dans un futur proche. </p>
+    <p>L’Utilisateur est informé que lors de ses visites sur le site, un cookie peut s’installer automatiquement sur son logiciel de navigation.</p>
+    <p>Les cookies sont de petits fichiers stockés temporairement sur le disque dur de l’ordinateur de l’Utilisateur par votre navigateur et qui sont nécessaires à l’utilisation du site d'Ub'Covoit. Les cookies ne contiennent pas d’information personnelle et ne peuvent pas être utilisés pour identifier quelqu’un. Un cookie contient un identifiant unique, généré aléatoirement et donc anonyme. Certains cookies expirent à la fin de la visite de l’Utilisateur, d’autres restent.</p>
+    <p> L’information contenue dans les cookies est utilisée pour améliorer le site d'Ub'Covoit.</p>
+    <p> En naviguant sur le site, l’Utilisateur les accepte.</p>
+
+    <br>
+    <p> L’Utilisateur pourra désactiver ces cookies par l’intermédiaire des paramètres figurant au sein de son logiciel de navigation.</p>
+
+
+    <h2>ARTICLE 8 : Publication par l’Utilisateur</h2>
+
+    <p>Le site permet aux membres de publier les contenus suivants :</p>
+    <p> Des rendez-vous entre plusieurs utilisateurs..</p>
+    <p> Dans ses publications, le membre s’engage à respecter les règles de la Netiquette (règles de bonne conduite de l’internet) et les règles de droit en vigueur.</p>
+    <p> Le site peut exercer une modération sur les publications et se réserve le droit de refuser leur mise en ligne, sans avoir à s’en justifier auprès du membre.</p>
+    <p>Le membre reste titulaire de l’intégralité de ses droits de propriété intellectuelle. Mais en publiant une publication sur le site, il cède à la société éditrice le droit non exclusif et gratuit de représenter, reproduire, adapter, modifier, diffuser et distribuer sa publication, directement ou par un tiers autorisé, dans le monde entier, sur tout support (numérique ou physique), pour la durée de la propriété intellectuelle. Le Membre cède notamment le droit d'utiliser sa publication sur internet et sur les réseaux de téléphonie mobile.</p>
+    <p>La société éditrice s'engage à faire figurer le nom du membre à proximité de chaque utilisation de sa publication.</p>
+    <p>Tout contenu mis en ligne par l'Utilisateur est de sa seule responsabilité. L'Utilisateur s'engage à ne pas mettre en ligne de contenus pouvant porter atteinte aux intérêts de tierces personnes. Tout recours en justice engagé par un tiers lésé contre le site sera pris en charge par l'Utilisateur.</p>
+    <p> Le contenu de l'Utilisateur peut être à tout moment et pour n'importe quelle raison supprimé ou modifié par le site, sans préavis.</p>
+    
+
+    <h2>ARTICLE 9 : Droit applicable et juridiction compétente</h2>
+
+    <p>La législation française s'applique au présent contrat. En cas d'absence de résolution amiable d'un litige né entre les parties, les tribunaux français seront seuls compétents pour en connaître.</p>
+    <p>Pour toute question relative à l’application des présentes CGU, vous pouvez joindre l’éditeur aux coordonnées inscrites à l’ARTICLE 1.</p>
+
+
+
+    <style>
+    <?php include 'css/conditions.css'; ?>
+    </style>
+</div>
 <?php
 include 'footer.php';
 ?>
